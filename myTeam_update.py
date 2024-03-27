540d880ec5705748696d2e92440e77452eb4c996
Joannewang-01 <159272346+Joannewang-01@users.noreply.github.com>
2024-03-27 03:28:12 +0100

    Update

--- myTeam_update.py
+++ myTeam_update.py
@@ -1,10 +1,10 @@
 # myTeam_update.py
-# ---------
+# ---------------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-#
+# 
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -45,6 +45,7 @@ def create_team(first_index, second_index, isRed,
     team, initialized using firstIndex and secondIndex as their agent
     index numbers.  isRed is True if the red team is being created, and
     will be False if the blue team is being created.
+
     As a potentially helpful development aid, this function can take
     additional string-valued keyword arguments ("first" and "second" are
     such arguments in the case of this function), which will come from
